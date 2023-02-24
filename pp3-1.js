af080b7abf773d566b7026f9c8091577f25a8386
mim2041 <suraiyamim338@gmail.com>
2023-02-24 22:30:39 +0600

    practice problem of finging an object property and arrow function

---
+++ pp3-1.js
@@ -0,0 +1,16 @@
+const div = a => a/5;
+const result = div(10);
+// console.log(result);
+
+const add = (a,b) => {
+    const add1 = a + 2;
+    const add2 = b + 2;
+    return add1 * add2;
+}
+
+const addition = add(4,6);
+// console.log(addition);
+
+const multi = (a,b,c) => a*b*c;
+const multiplication = multi(2,4,7);
+console.log(multiplication);
